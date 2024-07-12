f372c5bc0662bd75a9c8bc3e7eb573c1c6e5a2d2
cxfksword <718792+cxfksword@users.noreply.github.com>
2024-07-12 21:42:52 +0800

    update build

--- scripts/XCFrameworkBuild/main.swift
+++ scripts/XCFrameworkBuild/main.swift
@@ -4,10 +4,6 @@ do {
     let options = try ArgumentOptions.parse(CommandLine.arguments)
     try Build.performCommand(options)
     
-        // // enable-openssl
-    try BuildOpenSSL().buildALL()
-    try BuildDovi().buildALL()
-    try BuildVulkan().buildALL()
     try BuildShaderc().buildALL()
 } catch {
     print(error.localizedDescription)
@@ -16,17 +12,11 @@ do {
 
 
 enum Library: String, CaseIterable {
-    case libshaderc, vulkan, libdovi, openssl
+    case libshaderc
     var version: String {
         switch self {
         case .libshaderc:  // compiling GLSL (OpenGL Shading Language) shaders into SPIR-V (Standard Portable Intermediate Representation - Vulkan) code
             return "v2024.1"
-        case .openssl:
-            return "3.2.0"
-        case .libdovi:
-            return "v3.3.0"
-        case .vulkan:
-            return "1.2.9"
         }
     }
 
@@ -34,64 +24,6 @@ enum Library: String, CaseIterable {
         switch self {
         case .libshaderc:
             return "https://github.com/google/shaderc"
-        case .openssl:
-            return "https://github.com/mpvkit/openssl-build/releases/download/\(self.version)/openssl-all.zip"
-        case .libdovi:
-            return "https://github.com/mpvkit/libdovi-build/releases/download/\(self.version)/libdovi-all.zip"
-        case .vulkan:
-            return "https://github.com/mpvkit/moltenvk-build/releases/download/\(self.version)/MoltenVK-all.zip"
-        }
-    }
-}
-
-private class BuildOpenSSL: ZipBaseBuild {
-    init() {
-        super.init(library: .openssl)
-    }
-}
-
-
-private class BuildDovi: ZipBaseBuild {
-    init() throws {
-        super.init(library: .libdovi)
-    }
-}
-
-
-private class BuildVulkan: ZipBaseBuild {
-    init() {
-        super.init(library: .vulkan)
-    }
-
-    override func buildALL() throws {
-        try? FileManager.default.removeItem(at: URL.currentDirectory + library.rawValue)
-        try? FileManager.default.removeItem(at: directoryURL.appendingPathExtension("log"))
-        try? FileManager.default.createDirectory(atPath: (URL.currentDirectory + library.rawValue).path, withIntermediateDirectories: true, attributes: nil)
-        for platform in BaseBuild.platforms {
-            for arch in architectures(platform) {
-                // restore lib
-                let srcThinLibPath = directoryURL + ["lib", "MoltenVK.xcframework", platform.frameworkName]
-                let destThinPath = thinDir(platform: platform, arch: arch)
-                let destThinLibPath = destThinPath + ["lib"]
-                try? FileManager.default.createDirectory(atPath: destThinPath.path, withIntermediateDirectories: true, attributes: nil)
-                try? FileManager.default.copyItem(at: srcThinLibPath, to: destThinLibPath)
-
-                // restore include
-                let srcIncludePath = directoryURL + ["include"]
-                let destIncludePath = destThinPath + ["include"]
-                try? FileManager.default.copyItem(at: srcIncludePath, to: destIncludePath)
-
-                // restore pkgconfig
-                let srcPkgConfigPath = directoryURL + ["pkgconfig-example", platform.rawValue, arch.rawValue]
-                let destPkgConfigPath = destThinPath + ["lib", "pkgconfig"]
-                try? FileManager.default.copyItem(at: srcPkgConfigPath, to: destPkgConfigPath)
-                Utility.listAllFiles(in: destPkgConfigPath).forEach { file in
-                    if let data = FileManager.default.contents(atPath: file.path), var str = String(data: data, encoding: .utf8) {
-                        str = str.replacingOccurrences(of: "/path/to/workdir", with: URL.currentDirectory.path)
-                        try! str.write(toFile: file.path, atomically: true, encoding: .utf8)
-                    }
-                }
-            }
         }
     }
 }
@@ -100,7 +32,7 @@ private class BuildVulkan: ZipBaseBuild {
 private class BuildShaderc: BaseBuild {
     init() {
         super.init(library: .libshaderc)
-        try! Utility.launch(executableURL: directoryURL + "/utils/git-sync-deps", arguments: [], currentDirectoryURL: directoryURL)
+        try! Utility.launch(executableURL: directoryURL + "utils/git-sync-deps", arguments: [], currentDirectoryURL: directoryURL)
         var path = directoryURL + "third_party/spirv-tools/tools/reduce/reduce.cpp"
         if let data = FileManager.default.contents(atPath: path.path), var str = String(data: data, encoding: .utf8) {
             str = str.replacingOccurrences(of: """
@@ -143,6 +75,23 @@ private class BuildShaderc: BaseBuild {
             """)
             try! str.write(toFile: path.path, atomically: true, encoding: .utf8)
         }
+
+        // change libshaderc_combined.pc to libshaderc.pc as default pkgconfig file
+        // otherwise [libplacebo] will load shaderc failed
+        path = directoryURL + "CMakeLists.txt"
+        if let data = FileManager.default.contents(atPath: path.path), var str = String(data: data, encoding: .utf8) {
+            str = str.replacingOccurrences(of: """
+            define_pkg_config_file(shaderc -lshaderc_shared)
+            """, with: """
+            define_pkg_config_file(shaderc_shared -lshaderc_shared)
+            """)
+            str = str.replacingOccurrences(of: """
+            define_pkg_config_file(shaderc_combined -lshaderc_combined)
+            """, with: """
+            define_pkg_config_file(shaderc -lshaderc_combined)
+            """)
+            try! str.write(toFile: path.path, atomically: true, encoding: .utf8)
+        }
     }
 
     override func arguments(platform: PlatformType, arch: ArchType) -> [String] {
@@ -161,5 +110,7 @@ private class BuildShaderc: BaseBuild {
     override func frameworks() throws -> [String] {
         ["libshaderc_combined"]
     }
-    
+
+    override func buildALL() throws {
+    }
 }
